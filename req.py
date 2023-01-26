55da05c222539d26a3477bf0b27284738841a1b1
root <fakhari.ali@gmail.com>
2023-01-27 00:24:19 +0330

    request

--- req.py
+++ req.py
@@ -1,3 +1,7 @@
 import requests
 
-requests.get("https://tamin.ir")
+response = requests.get("https://gitlab.com/api/v4/users/9656669/projects")
+my_project = response.json()
+
+for anyproject in my_project:
+    print(f"Project Name is {anyproject['name']}, Project URL is {anyproject['web_url']}")
